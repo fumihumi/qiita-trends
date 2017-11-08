01cc130b2af8a5bd450a226ac33978898fd046aa
fumihumi <bgts1260011@gn.iwasaki.ac.jp>
2017-11-08 09:23:36 +0900

    don't use strftime.... '%Y/%M/%D'だとディレクトリとして認識されてしまった。。。。

--- qiita_feed.rb
+++ qiita_feed.rb
@@ -9,7 +9,7 @@ require 'open-uri'
 #define 
 WORKINGDIR = '~/qiita'
   #日時を取得
-DATE = Date.today.strftime("%Y/%m/%d")
+DATE = Date.today.to_s.gsub('-','')
 
 def get_doc
   url = 'https://qiita.com/popular-items'
@@ -18,12 +18,8 @@ def get_doc
      charset = f.charset # 文字種別を取得
      f.read # htmlを読み込んで変数htmlに渡す
   end
-  return  doc = Nokogiri::HTML.parse(html, nil, charset)
+  return  document = Nokogiri::HTML.parse(html, nil, charset)
 end
-
-# #main script
-sh = Shell.new
-sh.mkdir("#{WORKINGDIR}") unless sh.exists?("#{WORKINGDIR}")
 
 # #---------------------------------------------------
 #scraping
@@ -40,6 +36,11 @@ doc.xpath("//a[@class='popularItem_articleTitle_text']").each do |element|
   return_text += "#{text} \n"
 end
 # # 書き込み処理 #-------------------------------------------------
+
+# #main script
+sh = Shell.new
+sh.mkdir("#{WORKINGDIR}") unless sh.exists?("#{WORKINGDIR}")
+
 sh.cd("#{WORKINGDIR}") do
   f = sh.open("#{DATE}.md", "w")
   f.puts return_text
